13afa5173f0ab9dd359e7919af509b3e43ce33e4
Bruno do Carmo Souza <126525394+BrunoCarmoS2004@users.noreply.github.com>
2024-02-09 14:01:20 -0300

    Deixando dinamico algumas coisas

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './small-card.component.css'
 })
 export class SmallCardComponent {
+  @Input()
+  smallPhoto:string=''
+  @Input()
+  smallTitle:string=''
+  @Input()
+  smallDate:string=''
+  constructor(){
+
+  }
 
 }
